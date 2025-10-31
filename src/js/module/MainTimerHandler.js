7251983c129fed016964709d459ffbd2cd364ce2
dlwlrmanow <lullulalla9711@gmail.com>
2025-10-31 13:06:12 +0900

    edit: 디렉토리 이름 변경, todo layout추가, gnb css 수정

--- src/js/module/MainTimerHandler.js
+++ src/js/module/MainTimerHandler.js
@@ -5,6 +5,7 @@ import {AuthService} from "../services/AuthService.js";
 })();
 document.addEventListener('DOMContentLoaded', async () => {
     const logoutBtn = document.getElementById('logoutBtn');
+    const thingsTodoBtn = document.getElementById('thigsTodoBtn');
 
     if (logoutBtn) {
         logoutBtn.addEventListener('click', async (event) => {
@@ -14,12 +15,20 @@ document.addEventListener('DOMContentLoaded', async () => {
                 await AuthService.logout();
 
                 // token 무효화 후 페이지 이동
+                // 뒤로가기 불가
                 window.location.replace('http://localhost:63342/zipjung_web/src/templates/main.html');
             } catch (e) {
                 // TODO: 로그아웃 실패 모달
                 console.error('[MainTimerHandler] logout 실패;')
                 alert('로그아웃 처리 중 문제가 발생했습니다. 다시 시도해 주세요.');
             }
+        })
+    }
+
+    if (thingsTodoBtn) {
+        thingsTodoBtn.addEventListener('click', async (event) => {
+            event.preventDefault();
+            window.location.href = '../templates/todo/todo.html';
         })
     }
 
